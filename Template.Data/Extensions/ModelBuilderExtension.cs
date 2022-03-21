99fbe91ff919572757b8692d94ace81c642218eb
carlos.mello <carlos.mello@seventh.com.br>
2022-03-21 19:27:58 -0300

    install and setting Entity Freamework Code with .Net Core.

---
+++ Template.Data/Extensions/ModelBuilderExtension.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Template.Domain.Entities;
+
+namespace Template.Data.Extensions
+{
+    public static class ModelBuilderExtension
+    {
+        public static ModelBuilder SeedData(this ModelBuilder builder)
+        {
+            builder.Entity<User>()
+                .HasData(
+                   new User { Id = Guid.Parse("4D57EF40-3BEB-4D24-97AF-7E57EB71CA77"), Name = "User Default", Email = "UserDefault@templat.com" }
+                );
+
+            return builder;
+        }
+    }
+}
---
+++ Template.Data/Migrations/20220321222438_Insertind Default User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Template.Data.Migrations
+{
+    public partial class InsertindDefaultUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "Name" },
+                values: new object[] { new Guid("4d57ef40-3beb-4d24-97af-7e57eb71ca77"), "UserDefault@templat.com", "User Default" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("4d57ef40-3beb-4d24-97af-7e57eb71ca77"));
+        }
+    }
+}
